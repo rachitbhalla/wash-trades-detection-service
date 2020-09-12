97234459844d68d6678f0116296beeb8f4084e0b
Rachit Bhalla <rachitbhalla1998@gmail.com>
2020-09-13 01:02:39 +0530

    Initial Commit

---
+++ src/main/java/com/tradesurveillance/washtradesdetectionservice/WashTradesDetectionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tradesurveillance.washtradesdetectionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WashTradesDetectionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WashTradesDetectionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tradesurveillance/washtradesdetectionservice/WashTradesDetectionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradesurveillance.washtradesdetectionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WashTradesDetectionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
